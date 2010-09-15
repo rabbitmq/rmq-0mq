a879b9434ab9a4a249b6dd1301a2fb7459e534e7
Michael Bridgen <mikeb@squaremobius.net>
2010-09-15 11:10:01 +0100

    The example services uniformly use bind, so use connect in the clients

--- examples/pubsub/sub.py
+++ examples/pubsub/sub.py
@@ -3,7 +3,7 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.bind("tcp://127.0.0.1:5555")
+socket.connect("tcp://127.0.0.1:5556")
 socket.setsockopt (zmq.SUBSCRIBE, "")
 
 while True:
